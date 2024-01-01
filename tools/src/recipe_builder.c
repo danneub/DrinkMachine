96808eaaa01b2643e6c2359d1bcc01c25a548cd4
danneub <dhneuburger@gmail.com>
2024-01-01 17:22:54 -0500

    update recipe builder for alias

--- tools/src/recipe_builder.c
+++ tools/src/recipe_builder.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 
-const char * getfield(char* line,int num)
+const char * getfield(char* line,int num,char* noValueString)
 {
     char* found;
 
@@ -16,7 +16,7 @@ while(1)
        if (found[0] ==0)
        {
          
-         return  "0";
+         return  noValueString;
        }
        else
        {
@@ -41,35 +41,82 @@ int main()
             { line[i]=tolower(line[i]); }
 
            char * tmp = strdup(line);
-           printf("  '%s': [\n",getfield(tmp,1));
+           printf("  '%s': [\n",getfield(tmp,1,"0"));
            free (tmp);
            tmp = strdup(line);
-           printf("    { 'bottle' : 0, 'proportion': %s },\n",getfield(tmp,2));
+           printf("    { 'bottle' : 0, 'proportion': %s },\n",getfield(tmp,2,"0"));
            free (tmp);
            tmp = strdup(line);
-           printf("    { 'bottle' : 1, 'proportion': %s },\n",getfield(tmp,3));
+           printf("    { 'bottle' : 1, 'proportion': %s },\n",getfield(tmp,3,"0"));
            free (tmp);
            tmp = strdup(line);
-           printf("    { 'bottle' : 2, 'proportion': %s },\n",getfield(tmp,4));
+           printf("    { 'bottle' : 2, 'proportion': %s },\n",getfield(tmp,4,"0"));
            free (tmp);
            tmp = strdup(line);
-           printf("    { 'bottle' : 3, 'proportion': %s },\n",getfield(tmp,5));
+           printf("    { 'bottle' : 3, 'proportion': %s },\n",getfield(tmp,5,"0"));
            free (tmp);
            tmp = strdup(line);
-           printf("    { 'bottle' : 4, 'proportion': %s },\n",getfield(tmp,6));
+           printf("    { 'bottle' : 4, 'proportion': %s },\n",getfield(tmp,6,"0"));
            free (tmp);
            tmp = strdup(line);
-           printf("    { 'bottle' : 5, 'proportion': %s },\n",getfield(tmp,7));
+           printf("    { 'bottle' : 5, 'proportion': %s },\n",getfield(tmp,7,"0"));
            free (tmp);
            tmp = strdup(line);
-           printf("    { 'bottle' : 6, 'proportion': %s },\n",getfield(tmp,8));
+           printf("    { 'bottle' : 6, 'proportion': %s },\n",getfield(tmp,8,"0"));
            free (tmp);
            tmp = strdup(line);
-           printf("    { 'bottle' : 7, 'proportion': %s },\n",getfield(tmp,9));
+           printf("    { 'bottle' : 7, 'proportion': %s },\n",getfield(tmp,9,"0"));
+           free (tmp);
+           tmp = strdup(line);
+           printf("    { 'bottle' : 8, 'proportion': %s },\n",getfield(tmp,10,"0"));
            free (tmp);
            printf("  ],\n");
         }
+        fclose(stream);
 
+	stream = fopen("recipes.csv","r");
 
+        printf("MENU_ALIAS = {\n"); 
+        while (fgets(line,1024, stream))
+        {
+           for(int i =0;line[i];i++)
+            { line[i]=tolower(line[i]); }
+
+           char * tmp = strdup(line);
+           printf("  '%s': [\n",getfield(tmp,1,"0"));
+           free (tmp);
+           tmp = strdup(line);
+           free (tmp);
+           tmp = strdup(line);
+           free (tmp);
+           tmp = strdup(line);
+           free (tmp);
+           tmp = strdup(line);
+           free (tmp);
+           tmp = strdup(line);
+           free (tmp);
+           tmp = strdup(line);
+           free (tmp);
+           tmp = strdup(line);
+           free (tmp);
+           tmp = strdup(line);
+           free (tmp);
+           tmp = strdup(line);
+           free (tmp);
+           tmp = strdup(line);
+           printf("    { 'alias' : \"%s\" },\n",getfield(tmp,11,""));
+           free (tmp);
+           tmp = strdup(line);
+           printf("    { 'alias' : \"%s\" },\n",getfield(tmp,12,""));
+           free (tmp);
+           tmp = strdup(line);
+           printf("    { 'alias' : \"%s\" },\n",getfield(tmp,13,""));
+           free (tmp);
+           tmp = strdup(line);
+           printf("    { 'alias' : \"%s\" },\n",getfield(tmp,14,""));
+           free (tmp);
+           printf("  ],\n");
+        }
+        fclose(stream);
 }
 
